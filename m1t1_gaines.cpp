c72faa8a80cdf94609959917d76ee9dff15c4d27
purgepluto <babyplutoo09@gmail.com>
2025-01-27 19:03:53 +0000

    M1T1 Finished

---
+++ m1t1_gaines.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// M1T1 - Hello World
+// gaines
+// 1/27/2025
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello" <<endl;
+    cout << endl;
+    // write a few more cout statements 
+    // about you and things you like 
+cout << "I'm Jayden Gaines, I learn at FTCC" <<endl;
+cout << "I watch and read alot of Blue Lock" <<endl;
+cout << "I like chicken alfredo" <<endl;
+return 0; // no errors
+}
+
+
