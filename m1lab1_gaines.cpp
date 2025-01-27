345295471a131b4790ae81ae0fe7e79ac5d3c541
purgepluto <babyplutoo09@gmail.com>
2025-01-27 19:38:19 +0000

    M1LAB1 NOT FINISHED

---
+++ m1lab1_gaines.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M1LAB1 - Lemonade Stand
+// gaines
+// 1/27/2025
+// Today we're selling leomonade
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Declare your variables (boxes to holdl data)
+    string name = "Goat";               // string hold words, sentences, names, ... 
+    int num_CupsOfLemonade = 55;       // int are whole numbers (no decimal)
+    double price_each = 0.30;          // aany number with a decimal is a double 
+
+// Say hi
+cout <<"Welcome to the " << name << " lemonade stand." <<endl
+}
